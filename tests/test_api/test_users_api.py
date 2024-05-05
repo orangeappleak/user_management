048357c5475a4174354388005d30b7cb40005f44
orangeappleak <appannagarikarthik.123@gmail.com>
2024-05-05 13:27:40 -0400

    fixed connection issues

--- tests/test_api/test_users_api.py
+++ tests/test_api/test_users_api.py
@@ -83,12 +83,13 @@ async def test_create_user_invalid_email(async_client):
 
 
 @pytest.mark.asyncio
-async def test_create_user(async_client):
+async def test_create_user(async_client,admin_token):
+    headers = {"Authorization": f"Bearer {admin_token}"}
     data = {"email": "jon.does@example.com",
         "password": "AnotherPassword123!",
         "role" : "ANONYMOUS"}
-    response = await async_client.post("/register/", json=data)
-    assert response.status_code == 200
+    response = await async_client.post("/users/", json=data,headers=headers)
+    assert response.status_code == 201
     assert response.json()["email"] == data["email"]
 
 
